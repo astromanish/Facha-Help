ca3bcfbec2ea0b6b935e5187f5bf4e10a92374e8
Manish Singh <astromanishsingh@gmail.com>
2020-09-09 10:55:16 +0530

    Navbar,Homepage and Footer component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from './components/navbar'
+import Homepage from './components/homepage'
+import Footer from './components/footer'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
