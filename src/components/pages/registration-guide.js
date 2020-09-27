a4d8b2509a1fe26a5a07a3a6fbb140e4833b0b0b
Manish Singh <astromanishsingh@gmail.com>
2020-09-28 01:00:58 +0600

    registration guide added (but not workng)

---
+++ src/components/pages/registration-guide.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RegistrationGuide = () => {
+  return (
+    <>
+      <p>to be added after official registration details relesed</p>
+    </>
+  );
+};
+
+export default RegistrationGuide;
